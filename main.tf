6a317645d74dc6dccbe43f50ece8d2944bb18846
anil <anilreddy@gmail.com>
2023-08-13 13:02:00 +0530

    added chnag1

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ resource "aws_vpc" "ipc" {
 
 }
 
+resource "aws_subnet" "subnet1" {
+    vpc_id = aws_vpc.ipc.id
+    cidr_block = "10.10.6.0/24"
+
+  
+}
+
 resource "aws_subnet" "subnets" {
   count      = length(var.subnet_names)
   vpc_id     = aws_vpc.ipc.id
@@ -15,6 +22,36 @@ resource "aws_subnet" "subnets" {
     Name = var.subnet_names[count.index]
   }
 
+}
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.ipc.id
+
+  tags = {
+    name = "igway"
+  }
+
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.ipc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+
+  }
+
+  tags = {
+    name = "awsrt"
+  }
+
+  depends_on = [aws_internet_gateway.ig]
+}
+
+resource "aws_route_table_association" "rts" {
+    subnet_id = aws_subnet.subnet1.id
+    route_table_id = aws_route_table.rt.id  
 }
 
 
--- securitygroup.tf
+++ securitygroup.tf
@@ -7,3 +7,33 @@ resource "aws_security_group" "sg" {
     name = "security group"
   }
 }
+
+resource "aws_security_group_rule" "http" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.sg.id
+  
+}
+
+resource "aws_security_group_rule" "ssh" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.sg.id
+  
+}
+
+resource "aws_security_group_rule" "app" {
+    type = "egress"
+    from_port = 0
+    to_port = 65535
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.sg.id
+  
+}
