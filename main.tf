524abd85b631fcfce8262828db83c99dc243b101
anil <anilreddy@gmail.com>
2023-08-13 11:20:56 +0530

    added changes

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "ipc" {
+  cidr_block = "10.10.0.0/16"
+
+  tags = {
+    name = "ipc"
+  }
+
+}
+
--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
 provider "aws" {
-    region = "ap-south-1"
-  
-}
-
-
-resource "aws_vpc" "ipc" {
-    cidr_block = "10.10.0.0/16"
-    
-    tags = {
-      name = "ipc"
-    }
-  
+  region = "ap-south-1"
 }
