1fd90415e574c080e12369ba50cc43531fcbb9fe
anil <anilreddy@gmail.com>
2023-08-13 12:07:22 +0530

    added format

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "ipc" {
 resource "aws_subnet" "subnets" {
   count      = length(var.subnet_names)
   vpc_id     = aws_vpc.ipc.id
-  cidr_block = cidrsubnet(var.aws_vpc, 8, count.index)
+  cidr_block = format(var.subnet_cidr_format, count.index)
   tags = {
     Name = var.subnet_names[count.index]
   }
